d8dc0059b31bf531ae03b3ccd72c3f48e894e15b
김희찬 <wsx0239@gmail.com>
2023-03-07 09:48:04 +0900

    Day2

---
+++ Day2/day2/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Garage from './Garage.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Garage />
+    </div>
+  );
+}
+
+export default App;
---
+++ Day2/day2/src/Garage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Car(props){
+    return <h2>I am a {props.brand}!</h2>;
+}
+
+function Garage(){
+    return(
+        <>
+            <h1>Who lives in my garage?</h1>
+            <Car brand="Ford"/>
+        </>
+    );
+}
+
+export default Garage;
