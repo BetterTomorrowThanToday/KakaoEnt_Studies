6cf2a3f71042a1b22359080a484ad2401255da11
김희찬 <wsx0239@gmail.com>
2023-03-15 13:49:20 +0900

    day3 and 4 update

---
+++ Day4/day4_hw1/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+// import Comment from './Component/Comment';
+import Comment from "./Component/CommentList";
+
+function App() {
+  const userLog = [
+    {id:1, name: "Kim", comment: "안녕하세요"},
+    {id:2, name: "John", comment:"하이"},
+    {id:3, name: "Gustav" ,comment:"할로"}
+  ]
+  return (
+    <>
+      {userLog.map( 
+        (user) => (<Comment key={user.id} name={user.name} comment={user.comment}/>) 
+      )}
+    </>
+  );
+}
+
+export default App;
