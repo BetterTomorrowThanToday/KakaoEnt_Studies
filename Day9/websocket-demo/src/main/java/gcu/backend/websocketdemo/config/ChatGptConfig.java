2c4c2b3057d00f221cc4646ae0ceafebe6508c03
김희찬 <92291084+BetterTomorrowThanToday@users.noreply.github.com>
2023-04-12 18:18:16 +0900

    Update ChatGptConfig.java

--- Day9/websocket-demo/src/main/java/gcu/backend/websocketdemo/config/ChatGptConfig.java
+++ Day9/websocket-demo/src/main/java/gcu/backend/websocketdemo/config/ChatGptConfig.java
@@ -3,7 +3,7 @@
 //public class ChatGptConfig {
 //    public static final String AUTHORIZATION = "Authorization";
 //    public static final String BEARER = "Bearer ";
-//    public static final String API_KEY = "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";
+//    public static final String API_KEY = "here put API key";
 //    public static final String MODEL = "text-davinci-003";
 //    public static final Integer MAX_TOKEN = 300;
 //    public static final Double TEMPERATURE = 0.0;
